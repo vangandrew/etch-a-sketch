8e4b37fa1840aefae20665d3c17c87d4318c26a5
Andrew Vang <andrw.vang@gmail.com>
2025-02-23 21:04:57 -0600

    Add conditional statement to fill when mouse over
    Added an if conditon to check if the event is over the notepad container
    and only fill the current coordinate when the condition is met. Much
    better accuracy in filling in only the notepad but still lagging/slow
    response time in filling the notepad container.

--- scripts.js
+++ scripts.js
@@ -4,13 +4,15 @@ const notepad = document.querySelector(".notepad");
 notepad.addEventListener("mouseover", (e) => {
     const newDiv = document.createElement("div");
     newDiv.style.position = "fixed";
-    newDiv.style.left = e.clientX + 'px';
-    newDiv.style.top = e.clientY + 'px';
-    newDiv.style.width = '10px';
-    newDiv.style.height = '10px';
-    newDiv.style.backgroundColor = 'lightblue';
-
-    notepad.appendChild(newDiv);
+    if (e.target.className === "notepad") {
+        newDiv.style.left = e.clientX + 'px';
+        newDiv.style.top = e.clientY + 'px';
+        newDiv.style.width = '10px';
+        newDiv.style.height = '10px';
+        newDiv.style.backgroundColor = 'lightblue';
+    
+        notepad.appendChild(newDiv);
+    }
 })
 
 notepad.addEventListener("mouseout", () => {
