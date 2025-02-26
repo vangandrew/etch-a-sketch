05fdb440053206817b44c5ab4157ac0268d18090
Andrew Vang <andrw.vang@gmail.com>
2025-02-25 21:44:31 -0600

    Add function to create grid onto notepad container

--- scripts.js
+++ scripts.js
@@ -2,6 +2,19 @@ const notepad = document.querySelector(".notepad");
 
 let isInside = false;
 
+function createGrid(input) {
+    if (input > 100) {
+        return prompt("100 is the max grid amount... Try again!")
+    }
+    const gridAmount = input * input;
+    for (let i = 0; i < gridAmount + 1; i++) {
+        const squareDiv = document.createElement("div");
+        notepad.append(squareDiv);
+    }
+}
+
+createGrid(16);
+
 notepad.addEventListener("mousemove", (e) => {
 
     isInside = true;
@@ -18,7 +31,6 @@ notepad.addEventListener("mousemove", (e) => {
     
         notepad.appendChild(newDiv);
     }
-
 
 })
 
